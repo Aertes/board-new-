8410b52dffa460cd6ab10bdf1021fe4137dcafa2
Aertes <mrxu1010@outlook.com>
2018-04-26 16:45:31 +0800

    webpack.prod.conf

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -31,7 +31,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env,
-      'BASE_URL':JSON.stringify("http://119.23.53.39:8080/dashboard/"),
+      'BASE_URL':JSON.stringify("http://47.100.119.227:9999/dashboard/"),
       'PROD':JSON.stringify(true),
       'DEV':JSON.stringify(false)
     }),
